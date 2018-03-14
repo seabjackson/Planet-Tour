9d8064d636e923a970b808ae9c2d6e0118199bd8
Seab <honeyboyseab@gmail.com>
2018-03-14 11:34:05 -0400

    initial commit
    starter files

---
+++ Planet Tour/AppConstants.swift
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2016 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import UIKit
+
+struct AppConstants {
+
+  static let bigLabelColor = UIColor(rgba: "#FFFFFF66")
+  static let appPrimaryColor = UIColor(rgba: "#FBB03B")
+  static let navBarBackground = UIColor(rgba: "#535E66")
+  static let navTintColor = UIColor(rgba: "#FBB03B")
+  static let detailTitleColor = UIColor(rgba: "#FFFFFF")
+  static let detailInfoColor = UIColor(rgba: "#CCCCCC")
+  static let subscribeBannerText = "Like Planet Tour?"
+  static let subscribeBannerButton = "Get our newsletter!"
+  static let subscribeVCText = "Want more astronomy facts? Sign up for our newsletter!"
+  static let subscribeVCButton = "Subscribe"
+  static let shouldWeIncludePluto = false
+  static let planetImageScaleFactor = 0.33
+}
